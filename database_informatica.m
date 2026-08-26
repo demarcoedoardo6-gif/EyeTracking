f2380bbaee5243958c6b3c3221aeb55c78341058
demarcoedoardo6-gif <demarcoedoardo6@gmail.com>
2026-08-26 18:29:22 +0200

    Add files via upload
    read tabelle

---
+++ database_informatica.m
@@ -0,0 +1,4 @@
+Tabella_Pazienti = readtable('paziente.csv');
+Tabella_Diagnosi = readtable('diagnosi.csv');
+Tabella_Acquisizione = readtable('acquisizione.csv');
+Tabella_metriche = readtable('metriche.csv');
