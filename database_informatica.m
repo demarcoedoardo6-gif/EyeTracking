e85146115d7303a2b54db76dfcf3972ecc30d50a
Giuseppe Salvo <giuseppesalvo2006@gmail.com>
2026-08-30 18:10:00 +0200

    Add files via upload
    aggiunta dell' ia e un grafico

--- database_informatica.m
+++ database_informatica.m
@@ -61,3 +61,73 @@ disp(' ');
 disp('ANALISI DEI DEFICIT OCULOMOTORI: PARKINSON vs CONTROLLO');
 disp(Analisi_Ipometria(:, {'Gruppo_sano_patologico', 'mean_Ampiezza_deg', 'mean_Velocita_Picco_deg_s'}));
 
+
+
+
+
+
+
+
+
+
+% FASE 4: SUPPORTO DECISIONALE CON MACHINE LEARNING (EXPLAINABLE AI)
+
+% Addestramento di un modello per prevedere automaticamente se un paziente appartiene al gruppo "Controllo" o "Parkinson/Alzheimer".
+
+% Preparazione dei Dati (Feature e Target) ---
+% Definizione dei parametri oculomotori che l'algoritmo deve studiare (Input)
+Variabili_Input = {'Latenza_ms', 'Error_Rate_Perc', 'Ampiezza_deg', 'Velocita_Picco_deg_s'};
+
+% Estrazioni dal Database_finale solo dei dati clinici (X) e la diagnosi (Y)
+X_Dati = Database_finale(:, Variabili_Input);
+Y_Etichette = Database_finale.Gruppo_sano_patologico;
+
+% Addestramento del Modello e uso MaxNumSplits per limitare la profondità dell'albero 
+Modello_Diagnostico = fitctree(X_Dati, Y_Etichette, 'MaxNumSplits', 5);
+
+disp(' ');
+disp('MODELLO DI MACHINE LEARNING ADDESTRATO CON SUCCESSO!');
+
+% Generazione il grafico ad albero per vedere le regole cliniche imparate
+view(Modello_Diagnostico, 'Mode', 'graph');
+
+
+
+
+
+
+
+
+
+%%  5.1 Generazione Tracciato Oculomotore Sintetico
+
+% Creazione dell'asse del tempo (1 secondo diviso in 1000 millisecondi)
+Tempo_ms = 1:1000; 
+
+% Impostazione dei parametri presi dal nostro Albero Decisionale
+Latenza_Sano = 250;  Ampiezza_Sano = 22; % Sano: parte presto, arriva in alto
+Latenza_Park = 480;  Ampiezza_Park = 12; % Parkinson: parte tardi, movimento corto (Ipometria)
+Latenza_Alz  = 550;  Ampiezza_Alz  = 20; % Alzheimer: parte tardissimo, ma movimento quasi normale
+
+% 3. Formula matematica (Sigmoide) per simulare lo scatto dell'occhio per fare la curva a S. "-Latenza" sposta l'inizio a destra (ritardo), mentre il divisore finale (es. /25) schiaccia la pendenza per simulare la lentezza motoria. 
+Onda_Sano = Ampiezza_Sano ./ (1 + exp(-(Tempo_ms - Latenza_Sano) / 10));
+Onda_Park = Ampiezza_Park ./ (1 + exp(-(Tempo_ms - Latenza_Park) / 25));
+Onda_Alz  = Ampiezza_Alz  ./ (1 + exp(-(Tempo_ms - Latenza_Alz)  / 12));
+
+% 4. Disegniamo il monitor clinico
+figure('Name', 'Tracciato Oculomotore', 'Color', [0.1 0.1 0.1]); 
+plot(Tempo_ms, Onda_Sano, 'Color', [0.2 0.9 0.4], 'LineWidth', 3); hold on; % Verde   %hold on serve per sovrapporle
+plot(Tempo_ms, Onda_Park, 'Color', [0.9 0.2 0.3], 'LineWidth', 3);          % Rosso
+plot(Tempo_ms, Onda_Alz,  'Color', [0.2 0.7 0.9], 'LineWidth', 3);          % Azzurro
+
+% 5. Formattazione con comandi grafici 
+set(gca, 'Color', [0.15 0.15 0.15], 'XColor', 'white', 'YColor', 'white');
+title('Simulazione Oculomotoria: Sano vs Parkinson vs Alzheimer', 'Color', 'white');
+xlabel('Tempo (millisecondi)', 'Color', 'white');
+ylabel('Posizione Occhio (gradi)', 'Color', 'white');
+legend({'Controllo (Sano)', 'Parkinson', 'Alzheimer'}, 'TextColor', 'white', 'Color', [0.2 0.2 0.2], 'Location', 'northwest');
+grid on;
+
+
+
+
