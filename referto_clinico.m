ccc3110f423ecca7038421669d2492443e336674
demarcoedoardo6-gif <demarcoedoardo6@gmail.com>
2026-08-31 22:40:07 +0200

    Add files via upload

--- referto_clinico.m
+++ referto_clinico.m
@@ -1,7 +1,6 @@
 function referto_clinico(Database_finale, ID_Paziente)
     
-    % 1. CONTROLLI DI SICUREZZA
-    % Controllo se mancano gli input (es. se premiamo Run per sbaglio)
+   % Controllo input mancanti
     if nargin < 2
         disp('Errore: Devi fornire sia il database che l''ID del paziente.');
         return;
@@ -13,8 +12,7 @@ function referto_clinico(Database_finale, ID_Paziente)
         return;
     end
 
-    % 2. FILTRAGGIO DEI DATI
-    % Isolo solo le righe del database uguali al paziente che ho scelto
+    % FILTRAGGIO DEI DATI
     is_paziente = string(Database_finale.ID_Paziente) == string(ID_Paziente);
     Dati_Paziente = Database_finale(is_paziente, :);
     
@@ -28,7 +26,7 @@ function referto_clinico(Database_finale, ID_Paziente)
     is_controllo = string(Database_finale.Gruppo_sano_patologico) == "Controllo";
     Controlli = Database_finale(is_controllo, :);
     
-    % 3. CREAZIONE DEL FILE DI TESTO
+    % CREAZIONE DEL FILE DI TESTO
     % Creo il file .txt con il nome del paziente e lo apro in scrittura ('w')
     nome_file = sprintf('Referto_%s.txt', string(ID_Paziente));
     fid = fopen(nome_file, 'w');
@@ -38,8 +36,8 @@ function referto_clinico(Database_finale, ID_Paziente)
     fprintf(fid, 'Paziente: %s\n', ID_Paziente);
     fprintf(fid, 'Gruppo Clinico: %s\n\n', string(Dati_Paziente.Gruppo_sano_patologico(1)));
     
-    % 4. CALCOLO E SCRITTURA DELLE METRICHE
-    fprintf(fid, '--- CONFRONTO METRICHE VS SANI ---\n');
+    % CALCOLO E SCRITTURA DELLE METRICHE
+    fprintf(fid, ' CONFRONTO METRICHE VS SANI \n');
     
     % Calcolo la latenza media ignorando eventuali errori del macchinario ('omitnan')
     lat_paz = mean(Dati_Paziente.Latenza_ms, 'omitnan');
@@ -51,8 +49,8 @@ function referto_clinico(Database_finale, ID_Paziente)
     err_ctrl = mean(Controlli.Error_Rate_Perc, 'omitnan');
     fprintf(fid, 'Tasso di Errore: %.1f%% (Media Controllo: %.1f%%)\n\n', err_paz, err_ctrl);
     
-    % 5. CONCLUSIONE AUTOMATICA
-    fprintf(fid, '--- CONCLUSIONE AUTOMATICA ---\n');
+    % CONCLUSIONE AUTOMATICA
+    fprintf(fid, ' CONCLUSIONE AUTOMATICA \n');
     
     % Regola clinica: se il paziente è più lento del 20% rispetto ai sani
     if lat_paz > lat_ctrl * 1.2
@@ -61,11 +59,10 @@ function referto_clinico(Database_finale, ID_Paziente)
         fprintf(fid, 'Profilo oculomotorio nei limiti della norma.\n');
     end
     
-    % 6. CHIUSURA E SALVATAGGIO
-    % Chiudo il file per liberare la memoria di MATLAB
+    % CHIUSURA FILE
     fclose(fid);
     
-    % Avviso finale nella console
+    % Avviso finale nella command window
     disp(['Referto creato con successo: ' nome_file]);
     
 end
